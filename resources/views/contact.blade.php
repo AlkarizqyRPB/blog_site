f7bf7ff467498b36a4e65af620d44e88ec2c83cc
AlkarizqyRPB <alkarizqyrestupb@gmail.com>
2024-12-11 20:58:49 +0700

    Restore cache

--- resources/views/contact.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-<x-slot:title>{{ $title }}</x-slot:title>
-  <h3 class="text-xl">Ini halaman Contact</h3>
-</x-layout>
--- resources/views/home.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-  <x-slot:title>{{ $title }}</x-slot:title>
-  <h3 class="text-xl">Ini halaman home page</h3>
-</x-layout>
